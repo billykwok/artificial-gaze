3c4cb87b648002840f593ca60e0a86d7ec6102d0
Billy Kwok <opensource@billykwok.me>
2021-12-04 17:54:08 -0800

    smoother motion

--- artificial-gaze.ino
+++ artificial-gaze.ino
@@ -2,13 +2,33 @@
 #include <Wire.h>
 #include <math.h>
 
-#define SERVOMIN 150
-#define SERVOMAX 600
+#define ANGLE_MIN 35
+#define ANGLE_MAX 145
 #define SERVO_FREQ 50
 
 #define EYE_INTERVAL_FT 1
 #define DISTANCE_TO_PARTICIPANT_FT 4
-#define CAMERA_MAX_ANGLE_RAD 1.9
+#define CAMERA_MAX_ANGLE_RAD 1.92
+#define SERVO_DELAY 150
+
+const static uint16_t SERVO_MIN[16] = {
+    75,  75,  55,  55,   // row 1
+    110, 110, 110, 110,  // row 2
+    90,  90,  90,  90,   // row 3
+    45,  100, 55,  55    // row 4
+};
+
+const static uint16_t SERVO_MAX[16] = {
+    440, 450, 465, 500,  // row 1
+    465, 465, 465, 465,  // row 2
+    465, 465, 465, 465,  // row 3
+    440, 465, 465, 460   // row 4
+};
+
+long mapAngle(int input, int outputMin, int outputMax) {
+  return map(constrain(input, ANGLE_MIN, ANGLE_MAX), 0, 180, outputMin,
+             outputMax);
+}
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -17,10 +37,20 @@ void setup() {
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);
+  // while (1) {
+  //   for (uint8_t i = 0; i < 16; ++i) {
+  //     pwm.setPWM(i, 0, mapAngle(ANGLE_MIN, SERVO_MIN[i], SERVO_MAX[i]));
+  //   }
+  //   delay(SERVO_DELAY * 50);
+  //   for (uint8_t i = 0; i < 16; ++i) {
+  //     pwm.setPWM(i, 0, mapAngle(ANGLE_MAX, SERVO_MIN[i], SERVO_MAX[i]));
+  //   }
+  //   delay(SERVO_DELAY * 50);
+  // }
   for (uint8_t i = 0; i < 16; ++i) {
-    pwm.setPWM(i, 0, map(90, 0, 180, SERVOMIN, SERVOMAX));
+    pwm.setPWM(i, 90, mapAngle(ANGLE_MAX, SERVO_MIN[i], SERVO_MAX[i]));
   }
-  delay(500);
+  delay(SERVO_DELAY);
 }
 
 void loop() {
@@ -40,26 +70,26 @@ void loop() {
     angle = 90;
     switch (i % 4) {
       case 0:
-        angle = 90 + (atan((distanceToCenterFt - 1.5) / (EYE_INTERVAL_FT * 4)) *
+        angle = 90 - (atan((distanceToCenterFt - 1.5) / (EYE_INTERVAL_FT * 4)) *
                       180 / PI);
         break;
       case 1:
-        angle = 90 + (atan((distanceToCenterFt - 0.5) / (EYE_INTERVAL_FT * 4)) *
+        angle = 90 - (atan((distanceToCenterFt - 0.5) / (EYE_INTERVAL_FT * 4)) *
                       180 / PI);
         break;
       case 2:
-        angle = 90 + (atan((distanceToCenterFt - 0.5) / (EYE_INTERVAL_FT * 4)) *
+        angle = 90 - (atan((distanceToCenterFt - 0.5) / (EYE_INTERVAL_FT * 4)) *
                       180 / PI);
         break;
       case 3:
-        angle = 90 + (atan((distanceToCenterFt - 1.5) / (EYE_INTERVAL_FT * 4)) *
+        angle = 90 - (atan((distanceToCenterFt - 1.5) / (EYE_INTERVAL_FT * 4)) *
                       180 / PI);
         break;
     }
-    pwm.setPWM(i, 0, map(angle, 0, 180, SERVOMIN, SERVOMAX));
+    pwm.setPWM(i, 0, mapAngle(angle, SERVO_MIN[i], SERVO_MAX[i]));
     Serial.print(String(i) + ": " + (angle) + ", ");
   }
   Serial.println("");
 
-  delay(500);
+  delay(SERVO_DELAY);
 }
--- main.py
+++ main.py
@@ -1,49 +1,54 @@
 from typing import List
 from typing_extensions import Final
-from imutils.video import VideoStream
 from numpy import integer
 from data import KeyPoint
+from imutils.video import VideoStream
 from ml import Movenet
 import imutils
 import cv2
 import utils
 import arduino
 
-WIDTH: Final = 320
+WIDTH: Final = 720
+
+def is_in_right_distance(keypoints: List[KeyPoint]) -> bool:
+    left_eye, right_eye, left_ear, right_ear, left_shoulder, right_shoulder, *_ = keypoints;
+    return abs(left_eye.coordinate.x - right_eye.coordinate.x) > 8 or abs(left_ear.coordinate.x - right_ear.coordinate.x) > 8 or abs(left_shoulder.coordinate.x - right_shoulder.coordinate.x) > 40
 
 def is_facing_camera(keypoints: List[KeyPoint]) -> bool:
     left_eye, right_eye, left_ear, right_ear, left_shoulder, right_shoulder, *_ = keypoints;
     return left_eye.coordinate.x >= right_eye.coordinate.x or left_ear.coordinate.x >= right_ear.coordinate.x or left_shoulder.coordinate.x >= right_shoulder.coordinate.x
 
 def main():
-    # serial = arduino.auto_connect()
+    serial = arduino.auto_connect()
     vs = VideoStream(src=0).start()
     pose_detector = Movenet('movenet_singlepose_thunder_float16')
     angle: integer = 127
     absence_count: integer = 0
 
     while True:
-        # line = serial.readline()
-        # if line:
-        #     print(line.decode('ascii'), end=None)
+        line = serial.readline()
+        if line:
+            print(line.decode('ascii'), end=None)
 
         frame = cv2.flip(imutils.resize(vs.read(), width=WIDTH), 1)
         person = pose_detector.detect(frame, True)
-        if person and person.score >= 0.5 and len(person.keypoints) >= 3:
+        if person and person.score >= 0.25 and len(person.keypoints) >= 8:
             absence_count = 0
             (nose, *rest) = person.keypoints;
 
-            new_angle: integer = angle
-            if is_facing_camera(rest):
-                new_angle = 0 if nose.coordinate.x <= WIDTH * 0.5 else 255
-            else:
-                new_angle = round(nose.coordinate.x * 255 / WIDTH)
+            if is_in_right_distance(rest):
+                new_angle: integer = angle
+                if is_facing_camera(rest):
+                    new_angle = 0
+                else:
+                    new_angle = round(nose.coordinate.x * 255 / WIDTH)
 
-            if new_angle != angle:
-                angle = new_angle
-                # print("Angle: {}", angle)
-                # serial.write((angle).to_bytes(1, 'little'))
-                # serial.flush()
+                if new_angle != angle:
+                    angle = new_angle
+                    print("Angle: {}", angle)
+                    serial.write((angle).to_bytes(1, 'little'))
+                    serial.flush()
 
             cv2.imshow('movenet_singlepose_thunder_float16', utils.visualize(frame, [person]))
 
@@ -53,8 +58,8 @@ def main():
         if absence_count >= 5:
             if 127 != angle:
                 angle = 127
-                # serial.write((angle).to_bytes(1, 'little'))
-                # serial.flush()
+                serial.write((angle).to_bytes(1, 'little'))
+                serial.flush()
 
             cv2.imshow('movenet_singlepose_thunder_float16', utils.visualize(frame, []))
 
